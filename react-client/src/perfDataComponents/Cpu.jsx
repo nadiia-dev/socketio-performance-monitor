faeb756b37c3a5fd93e4ae31176fdac2aba7da06
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-10 18:44:17 +0300

    finalize cpu widget

--- react-client/src/perfDataComponents/Cpu.jsx
+++ react-client/src/perfDataComponents/Cpu.jsx
@@ -1,5 +1,19 @@
-const Cpu = () => {
-  return <div className="cpu col-3">Cpu</div>;
+import { useRef } from "react";
+import drawCircle from "../utilities/canvasLoadAnimation";
+
+const Cpu = ({ data }) => {
+  const canvasRef = useRef();
+  drawCircle(canvasRef.current, data.cpuLoad);
+
+  return (
+    <div className="cpu col-3">
+      <h3>CPU Load</h3>
+      <div className="canvas-wrapper">
+        <canvas ref={canvasRef} className="" width="200" height="200"></canvas>
+        <div className="cpu-text">{data.cpuLoad}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Cpu;
